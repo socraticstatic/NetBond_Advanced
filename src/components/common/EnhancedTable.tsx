bfa32846241c1e828f4a7015ad17ec31d85fa6fe
CloudConsole <smackintosh@me.com>
2025-10-16 15:00:59 -0500

    Updated EnhancedTable.tsx

--- src/components/common/EnhancedTable.tsx
+++ src/components/common/EnhancedTable.tsx
@@ -112,7 +112,7 @@ function EnhancedTableComponent<T>({
 
   return (
     <div className="flex flex-col">
-      <div className="w-full overflow-x-auto bg-white rounded-lg border border-gray-200 overflow-hidden">
+      <div className="w-full overflow-x-auto overflow-hidden">
         <table className="w-full divide-y divide-gray-200">
           <thead className={`bg-gray-50 ${stickyHeader ? 'sticky top-0 z-10' : ''}`}>
             <tr>
