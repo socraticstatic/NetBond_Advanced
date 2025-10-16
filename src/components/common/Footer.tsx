db15cd4c48c978a09f0915fe8257642982072783
CloudConsole <smackintosh@me.com>
2025-10-16 13:15:24 -0500

    Updated Footer.tsx

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,9 +1,3 @@
-import { Shield, Activity } from 'lucide-react';
-
 export function Footer() {
-  return (
-    <footer>
-      {/* Footer content removed to eliminate visual separators */}
-    </footer>
-  );
+  return null;
 }
