bb3cd486cba7177271a54484f3ef394a444b0044
CloudConsole <smackintosh@me.com>
2025-11-07 14:47:53 -0600

    Updated MainNav.tsx

--- src/components/navigation/MainNav.tsx
+++ src/components/navigation/MainNav.tsx
@@ -165,7 +165,7 @@ export function MainNav({ items = [], onSearch }: MainNavProps) {
             >
               <div className="flex items-center">
                 <span className="text-xl font-bold text-fw-heading">AT&T</span>
-                <span className="ml-3 text-xl font-semibold text-fw-link">NetBond® Advanced</span>
+                <span className="ml-3 text-xl font-bold text-fw-link">NetBond® Advanced</span>
               </div>
             </div>
 
