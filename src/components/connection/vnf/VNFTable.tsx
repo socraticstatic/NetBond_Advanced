35a159ab7b35ef6f31c9dc15e9a3ddb9a85e8b7c
CloudConsole <smackintosh@me.com>
2025-11-08 16:52:05 -0600

    Updated VNFTable.tsx

--- src/components/connection/vnf/VNFTable.tsx
+++ src/components/connection/vnf/VNFTable.tsx
@@ -186,7 +186,7 @@ export function VNFTable({
       pageSize={50}
       showPagination={vnfs.length > 50}
       stickyHeader={true}
-      showExport={true}
+      showExport={false}
       tableId="vnf"
       showColumnManager={true}
       showFilter={true}
